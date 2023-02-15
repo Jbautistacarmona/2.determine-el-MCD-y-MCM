7df2d34baa3300c3a9dbb4a50c966d60d905d8cc
Jbautistacarmona <124100561+Jbautistacarmona@users.noreply.github.com>
2023-02-15 13:26:43 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.determine_el_MCD_y_MCM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Calcular MCD y MCM"); //mensaje en pantalla al usuario.
+            Console.WriteLine("--------------------");
+
+            Console.Write("Ingrese el primer número: ");//Convierte la representación en forma de cadena de un número en el entero de 32 bits con signo equivalente.
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese el segundo número: ");//Convierte la representación en forma de cadena de un número en el entero de 32 bits con signo equivalente.
+            int num2 = int.Parse(Console.ReadLine());
+
+            int gcd = GetGCD(num1, num2); //variable local
+            Console.WriteLine("\nEl MCD es: " + gcd);
+
+            int lcm = num1 * num2 / gcd;
+            Console.WriteLine("El MCM es: " + lcm); //imprimir mensaje en pantalla.
+
+            Console.WriteLine("\nPresione cualquier tecla para salir...");
+            Console.ReadKey();
+        }
+
+        static int GetGCD(int a, int b)
+        {
+            if (b == 0)//la bifurcación condicional. El cuerpo de una instrucción if se ejecuta si el valor de la expresión es distinto de cero.
+            {
+                return a;//nos devuelve el valor.
+            }
+            else//Es usada para ejecutar una instrucción o bloque de instrucciones solo si una condición es cumplida
+            {
+                return GetGCD(b, a % b);
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("2.determine el MCD y MCM")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("2.determine el MCD y MCM")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("89513606-6721-42bd-b9a5-2b4d70cc2ec0")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
